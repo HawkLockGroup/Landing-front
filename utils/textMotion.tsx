56b6f6ccef15c46963a2f7b7bbe2521783e87f1f
Arian <a.arian@Behsacorp.com>
2024-01-02 17:25:28 +0330

    textmotion component

---
+++ utils/textMotion.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
+interface TextMotionProps {
+  text: string;
+}
+
+export function TextMotion({ text }: TextMotionProps) {
+  const words = text.split(" ");
+  const container = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+    }),
+  };
+  // Variants for each word.
+  const child = {
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
+  return (
+    <motion.div
+      className="text-red-500"
+      style={{ overflow: "hidden", display: "flex", fontSize: "2rem" }}
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {words.map((word, index) => (
+        <motion.span
+          className="text-red-400"
+          variants={child}
+          style={{ marginRight: "5px" }}
+          key={index}
+        >
+          {word}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+}
